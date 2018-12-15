141f3d979e3784a58dc44b32b303d1fe65a1d578
xiaosi1 <18434842193@163.com>
2018-12-15 09:03:37 +0800

    这是一个学生类

--- src/main/java/com/tt/student/pojo/student.java
+++ src/main/java/com/tt/student/pojo/student.java
@@ -2,4 +2,12 @@ package com.tt.student.pojo;
 
 public class student {
      private String name;
+
+     public String getName() {
+          return name;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
 }
